594a28c765692e57e391cb6bf2b5c283370c43ce
arazin <mailaddress_20120316@me.com>
2013-12-04 17:26:15 +0900

    修正

--- newsqls/add.sql
+++ newsqls/add.sql
@@ -1,8 +1,8 @@
-alter table events add (created datetime NOT NULL,modified datetime NOT NULL);
+alter table events add (created datetime,modified datetime );
 alter table graduates add (lasteventview datetime default NULL);
-alter table students add (created datetime NOT NULL,modified datetime NOT NULL);
-alter table graduates add (created datetime NOT NULL,modified datetime NOT NULL);
-alter table participants add (created datetime NOT NULL,modified datetime NOT NULL);
+alter table students add (created datetime,modified datetime );
+alter table graduates add (created datetime,modified datetime );
+alter table participants add (created datetime,modified datetime );
 
 
 
