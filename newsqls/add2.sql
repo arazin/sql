2d6bafce0a95e1c7a3642d446192ddea45e013ee
arazin <mailaddress_20120316@me.com>
2013-12-05 09:26:56 +0900

    add2.sqlに修了生向けイベントテーブルを追加

---
+++ newsqls/add2.sql
@@ -0,0 +1,10 @@
+create table gevnents
+	id int unsigned auto_increment primary key,
+	title varchar(100) NOT NULL,
+	gevent_date date,
+	place text,
+	detail text,
+	created date,
+	modified date
+);
+
