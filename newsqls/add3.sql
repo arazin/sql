d56cebcaa31be063e18af2eb5e2dd57ffd43adc1
arazin <mailaddress@hoge.hoge>
2014-01-11 12:41:45 +0900

    add3.sql変更

--- newsqls/add3.sql
+++ newsqls/add3.sql
@@ -9,3 +9,20 @@ create table gevents(
 	created DATETIME,
 	modified DATETIME
 )ENGINE=InnoDB;
+
+drop table if exists careers;
+create table careers(
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	graduate_id INT UNSIGNED NOT NULL,
+	belong VARCHAR(100) NOT NULL,
+	posit VARCHAR(50) NOT NULL,
+	industry_id INT UNSIGNED NOT NULL,
+	special_report TEXT,
+	created DATETIME,
+	modified DATETIME,
+	foreign key(graduate_id) references graduates(user_id)
+	on update cascade on delete cascade,
+	foreign key(industry_id) references industries(id)
+	on update cascade on delete cascade
+)ENGINE=InnoDB;
+
